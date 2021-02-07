fa7b54c5a1c7c0dd5fa1eee6d8ab14b102176ea7
AbhiDhariwal <cool.ahbi@gmail.com>
2021-02-07 12:35:50 +0530

    Update exception handling

--- imagehackrun.py
+++ imagehackrun.py
@@ -22,14 +22,14 @@ app.add_middleware(
 
 @app.post("/items/")
 async def create_item(item: Item):
-	print(item)
-	try:
-		urllib.request.urlretrieve(item.url, "testing.jpg")
-		cap =  predict_captions("testing.jpg")
-		print ('Normal Max search:', cap) 
-	except Exception as e:
-		print("Error:",e)
-		cap = ""
+    print(item)
+    try:
+        urllib.request.urlretrieve(item.url, "testing.jpg")
+        cap =  predict_captions("testing.jpg")
+        print ('Normal Max search:', cap) 
+    except Exception as e:
+        print("Error:",e)
+        cap = ""
 
     return {"id":item.id, "url":item.url, "output":cap}
 
